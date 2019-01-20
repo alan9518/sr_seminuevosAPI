27918b3e5737834fecc3810dd6eb625ad638f668
Alan Medina <alan.medina@flex.com>
2019-01-20 14:06:18 -0600

    Relation between Marca & Model Created

--- app/Model/Marca.php
+++ app/Model/Marca.php
@@ -3,8 +3,12 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Modelo;
 
 class Marca extends Model
 {
-    //
+    // Add Relation With Modelo
+    public function Modelos() {
+        return $this->hasMany(Modelo::class);
+    }
 }
--- app/Model/Modelo.php
+++ app/Model/Modelo.php
@@ -3,8 +3,12 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Marca;
 
 class Modelo extends Model
 {
-    //
+    // Add Relation with Marca
+    public function Marca() {
+        return $this->belongsTo(Marca::class);
+    }
 }
